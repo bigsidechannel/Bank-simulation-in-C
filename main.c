e3e91f68fb632b53341e063c97a5b321935f7b60
Melih Baha Uygun <kanserojenfrizbi@gmail.com>
2026-09-24 11:25:08 +0300

    ilk commit

--- main.c
+++ main.c
@@ -1,7 +1,34 @@
 #include <stdio.h>
+#include <stdint.h>
 
-int main(void) {
-    printf("Hello, World!\n");
+typedef enum {
+    CHECKING,
+    SAVINGS,
+    INVESTMENT
+} Account;
+
+typedef struct {
+    uint8_t day;
+    uint8_t month;
+    uint16_t year;
+}Birthday;
+
+typedef struct {
+    char name[30];
+    char iban[27];
+    char tc[12];
+    uint32_t customer_id;
+    uint64_t balance;
+    uint64_t debt;
+    Account account_type;
+    Birthday birthday;
+        
+}Customer;
+
+
+
+
+int main() {
+    printf("");
     return 0;
 }
-    
